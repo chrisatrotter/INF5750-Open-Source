e047749676004d7d62a606e0eec52c6ac51e389e
ArselanSultani <arselans@ulrik.uio.no>
2016-10-29 23:55:46 +0200

    Stepper

---
+++ src/components/FirstStep.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FirstStep extends React.Component {
+  render()  {
+    return (
+      <div>
+        <p> First Step </p>
+      </div>
+    );
+  }
+}
+
+module.exports = FirstStep;
---
+++ src/components/ThirdStep.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ThirdStep = React.createClass({
+	render: function() {
+		return <p> ThirdStep </p>;
+	}
+});
+
+module.exports = ThirdStep;
