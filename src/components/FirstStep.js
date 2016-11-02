3f10d90b51a12dcf7c36a8f4cfd23910750636da
chrisat <chrisat@ifi.uio.no>
2016-11-02 18:16:18 +0100

    FirstStep.js - display hardcoded array of countries and sorted a filter search method.

--- src/components/FirstStep.js
+++ src/components/FirstStep.js
@@ -1,18 +1,69 @@
 //@flow
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
+import TextField from 'material-ui/TextField';
 
 class FirstStep extends React.Component {
+  state: {
+    input: string,
+  }
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+    this.getUserInput = this.getUserInput.bind(this);
+  }
+
+  getUserInput(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  onChange(event) {
+    this.setState({input: event.target.value});
+  }
+
+  getCountries(countries, input) {
+    return countries.filter(country => country.toLowerCase()
+                                              .startsWith(input.toLowerCase()))
+                    .map(country => (<ListItem primaryText={country} />));
+  }
+
+  fetchCountries() {
+    return fetch('http://api.dhsprogram.com/rest/dhs/countries.json?returnFields=CountryName,DHS_CountryCode')
+             .then(response => response.json())
+  }
+
   render()  {
+    var countries = [
+      "Afghanistan", "Albania", "Andorra",
+      "Belgium", "Belize", "Bermuda",
+      "Kenya",
+      "Nauru", "Norway",
+      "Morocco",
+      "Peru",
+      "Romania",
+      "Swaziland",
+      "Vanuatu", "Vatican City",
+      "Zimbabwe"];
+
     return (
       <div>
-        <p>Choose country:</p>
+        {console.log(this.fetchCountries().then(response => console.log(response.Data)))}
+
+        <p> First Step </p>
+
+        <TextField
+          hintText="Country"
+          fullWidth={true}
+          value={this.state.input}
+          onChange={this.getUserInput.bind(this)}
+        />
+
         <List>
-          <ListItem primaryText="Amerika" />
-          <ListItem primaryText="Brazil" />
-          <ListItem primaryText="Norge" />
-          <ListItem primaryText="India" />
-          <ListItem primaryText="Somewhere" />
+          {this.getCountries(countries, this.state.input)}
         </List>
       </div>
     );
