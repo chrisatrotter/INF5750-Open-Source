6dbcd3ce2774c170fba0c2c0ad77a9bb0b4591a5
Kien Vu <kienv91@gmail.com>
2016-11-23 22:34:22 +0100

    test for CountryStep

--- src/test/countryStep.test.js
+++ src/test/countryStep.test.js
@@ -1,12 +1,51 @@
-var expect = require('expect');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('INDEXOF', function() {
-      expect(true == true);
-    });
-  });
+//@flow
+import React from 'react'
+import expect from 'expect'
+import { shallow } from 'enzyme'
+import { CountryStep } from '../components/pages/CountryStep'
 
-  it('ARRAY', function() {
-    expect(true == true);
-  });
+describe('Component: CountryStep', () => {
+
+    const minProps = {
+      countries: [{CountryName: "Albania", DHS_CountryCode: "AL"}, {CountryName: "Bangladesh", DHS_CountryCode: "BD"}],
+      selectedCountry: "",
+      fetchCountries: () => {},
+      countrySelected: () => {},
+      stepIndex: 1,
+    }
+
+    it('renders without exploding', () => {
+      expect(
+        shallow(
+          <CountryStep {...minProps} />
+        ).length
+      ).toEqual(1)
+    })
+
+
+    it('renders a TextField with hintText: Country', () => {
+      const wrapper = shallow(<CountryStep {...minProps} />)
+      expect(
+        wrapper.find('TextField').prop('hintText')
+      ).toEqual('Country')
+    });
+
+    it('renders a ListButton with Albania and Bangladesh', () => {
+      const wrapper = shallow(<CountryStep {...minProps} />)
+      expect(
+        wrapper.find('ListButton').at(0).prop('label')
+      ).toEqual('Albania')
+      expect(
+        wrapper.find('ListButton').at(1).prop('label')
+      ).toEqual('Bangladesh')
+    });
+
+    it('should correctly render TextField', () => {
+      const wrapper = shallow(<CountryStep {...minProps} />)
+      wrapper.setState({ input: 'Albania' })
+      expect(
+        wrapper.find('TextField').props().value
+      ).toEqual('Albania')
+    })
+
 });
