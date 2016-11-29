9073066b64e6f8c180cfc46e17449f2cbddcc2e6
Christopher Alexander Trotter <chrisat@ifi.uio.no>
2016-11-29 14:16:33 +0100

    Import total dialog box

--- src/components/layout/Header/Logo.js
+++ src/components/layout/Header/Logo.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 export default class Logo extends Component {
   render() {
     return (
-      <img src={require('../../../../public/logo2.png')} role={"presentation"} style={this.props.logoStyle} />
+      <img src={require('../../../../public/logo.png')} role={"presentation"} style={this.props.logoStyle} />
     );
   }
 }
